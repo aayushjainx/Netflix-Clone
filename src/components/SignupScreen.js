e86bfbd28c424c1fb5746e882cef71b3767226c6
Aayush Jain <jaayush014@gmail.com>
2021-02-06 22:28:36 +0530

    Added SignIn/SignUp Screen

--- src/components/SignupScreen.js
+++ src/components/SignupScreen.js
@@ -2,13 +2,29 @@ import React from 'react';
 import '../styles/SignupScreen.css';
 
 function SignupScreen() {
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='signupScreen'>
       <form>
-        <h1>SignIn</h1>
+        <h1>Sign In</h1>
         <input placeholder='Email' type='email' />
         <input placeholder='Password' type='password' />
-        <button type='submit'>Sign In</button>
+        <button type='submit' onClick={signIn}>
+          Sign In
+        </button>
+        <h4>
+          <span className='signupScreen__gray'>New to Netflix? </span>
+          <span className='signupScreen__link' onClick={register}>
+            Sign Up now.
+          </span>
+        </h4>
       </form>
     </div>
   );
